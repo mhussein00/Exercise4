fed36f19ffdc5d43543186baa9ec710da3cb8712
mhussein00 <148893874+mhussein00@users.noreply.github.com>
2024-02-08 14:27:18 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,41 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static Map<Double, String> newMap = new HashMap<>();
+
+    public static ArrayList<String> cities = new ArrayList<>();
+    public static String promptUser(String city){
+        System.out.println("Please enter a city. Type END to stop.");
+        city = input.nextLine();;
+        return city;
+    }
+
+    public static void findandPrintAvg(String a) {
+        System.out.println("Enter Day 1 Temp for " + a);
+        double temp1 = input.nextDouble();
+        input.nextLine();
+        System.out.println("Enter Day 2 Temp for " + a);
+        double temp2 = input.nextDouble();
+        input.nextLine();
+        System.out.println("Enter Day 3 Temp for " + a);
+        double temp3 = input.nextDouble();
+        input.nextLine();
+        System.out.println("Enter Day 4 Temp for " + a);
+        double temp4 = input.nextDouble();
+        input.nextLine();
+        System.out.println("Enter Day 5 Temp for " + a);
+        double temp5 = input.nextDouble();
+        input.nextLine();
+        double avg = (temp1+temp2+temp3+temp4+temp5)/5;
+        System.out.println("The average temperature for " + a + " is " + avg);
+        newMap.put(Math.round(avg*100.0)/100.0, a);
+    }
     public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-        Map<Double, String> newMap = new HashMap<>();
-
-        ArrayList<String> cities = new ArrayList<>();
-
         System.out.println("Please enter a city. Type END to stop.");
         String city = input.nextLine();
         while(!(city.equals("END"))) {
-
             cities.add(city);
             System.out.println(cities);
             System.out.println("Please enter a city. Type END to stop.");
@@ -24,24 +47,7 @@ public class Main {
         }
 
         for(String a : cities) {
-            System.out.println("Enter Day 1 Temp for " + a);
-            double temp1 = input.nextDouble();
-            input.nextLine();
-            System.out.println("Enter Day 2 Temp for " + a);
-            double temp2 = input.nextDouble();
-            input.nextLine();
-            System.out.println("Enter Day 3 Temp for " + a);
-            double temp3 = input.nextDouble();
-            input.nextLine();
-            System.out.println("Enter Day 4 Temp for " + a);
-            double temp4 = input.nextDouble();
-            input.nextLine();
-            System.out.println("Enter Day 5 Temp for " + a);
-            double temp5 = input.nextDouble();
-            input.nextLine();
-            double avg = (temp1+temp2+temp3+temp4+temp5)/5;
-            System.out.println("The average temperature for " + a + " is " + avg);
-            newMap.put(Math.round(avg*100.0)/100.0, a);
+            findandPrintAvg(a);
         }
 
         System.out.println(newMap);
